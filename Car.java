c1d5e6d1bec7ad5a247bc9fb2d5abb4c9d6daa0a
essammaher10101 <essammaher10101@gmail.com>
2026-05-11 14:11:24 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,42 @@
+import javax.xml.crypto.Data;
+
+public class Car {
+    int CurrentSpeed = 0;
+    String color;
+    String model;
+    int year;
+    Data dateOfManufacture;
+
+    //This TO pass the current object as a parameter to another method
+    Car getCarDetails(){
+        return this;
+    }
+
+    Car(){
+
+    }
+    //This To refer to instance variables when they are shadowed by method parameters(same name)
+    Car (String color){
+        this.color = color;
+    }
+    //Overloaded constructor
+    //increasing the number of parameters
+    //Using this() to call another constructor in the same class
+    Car(String model, int year, String color){
+        this(color);
+        this.model = model;
+        this.year = year;
+    }
+    //Overloaded constructor
+    //Changing the order of parameters
+    Car(int year, String model){
+        this.year = year;
+        this.model = model;
+    }
+    void increaseSpeed(){
+        CurrentSpeed += 50;
+    }
+    void applyBrakes(){
+        CurrentSpeed -= 50;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Car car = new Car();
+        System.out.println(car.color);
+        Car car1 = new Car("Red");
+        System.out.println(car1.color);
+//        int number = 12345;
+//        int reversedNumber = reverse1(number);
+//        System.out.println("Original number: " + number);
+//        System.out.println("Reversed number: " + reversedNumber);
+    }
+
+
+
+
+    public static int reverse1(int num) {
+        if (num < 10) {
+            return num;
+        }
+        return (num % 10) * (int) Math.pow(10, (int) Math.log10(num)) + reverse1(num / 10);
+    }
+}
